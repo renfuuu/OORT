165ed6a2833023659540b96a18c16d9b7986dc78
Itoro Esiere <iturtle@cs.utexas.edu>
2016-04-28 17:51:14 -0500

    Started implementing Asteroids

---
+++ oort/Source/Core/Asteroid.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "GameObject.h"
+class Asteroid : public GameObject {
+
+public:
+	Asteroid(Ogre::String nme, GameObject::objectType tp, Ogre::SceneManager* scnMgr, GameManager* ssm, Ogre::SceneNode* node, Ogre::Entity* ent, OgreMotionState* ms, Simulator* sim, Ogre::Real mss, Ogre::Real rest, Ogre::Real frict, Ogre::Real scal, bool kin);
+	~Asteroid();
+
+	bool hitWall;
+	Ogre::Real velocity;
+
+	virtual void update();
+	void move();
+};
